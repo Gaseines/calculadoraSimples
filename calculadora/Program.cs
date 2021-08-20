d401cf2fae4aa3ee6afa7d8ddbe456225ff67d7d
Gaseines <83676306+Gaseines@users.noreply.github.com>
2021-08-19 22:37:40 -0300

    Dale

---
+++ calculadora/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite um número: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite um número: ");
+            Console.WriteLine(n1 + n2);
+        }
+    }
+}
---
+++ calculadora/ProgramBase1.cs
@@ -0,0 +1,22 @@
+﻿namespace calculadora
+{
+    internal class ProgramBase1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite um número: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite um número: ");
+            string s1 = (Console.ReadLine());
+
+            if (s1 == "+")
+            {
+                Console.WriteLine(n1 + n2);
+
+
+            }
+        }
+    }
+}
